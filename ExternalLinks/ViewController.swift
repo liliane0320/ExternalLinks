c8f8096132b458a42b1011b0098bd2b6683c5f44
李梓桐 <riburide@Macintosh-2.local>
2015-03-11 22:05:43 -0400

    Initial Commit

---
+++ ExternalLinks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExternalLinks
+//
+//  Created by 李梓桐 on 2015-03-11.
+//  Copyright (c) 2015 Liliane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
